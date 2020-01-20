768fa6281e808bbc666b9ffdb971f760fc00c161
Christopher <christopherjacobs1234@hotmail.com>
2020-01-20 08:57:57 +0100

    Initial commit

---
+++ product.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+import { Observable } from 'rxjs';
+import {of} from 'rxjs';
+
+
+@Injectable()
+export class ProductService {
+   products: Product[]
+
+   constructor() {
+   }
+
+   getAllProducts(): Product[] {
+      this.products=[
+         {
+               'name' : 'boek',
+               'brand': 'addidas',
+               'description':'boek over addidas',
+               'price':2
+         },
+         {
+            'name' : 'schoen',
+            'brand': 'addidas',
+            'description':'schoen van addidas',
+            'price':200
+         }
+      ]
+      return this.products
+   }
+}
